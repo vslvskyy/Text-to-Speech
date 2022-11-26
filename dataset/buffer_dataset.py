61e56ea72461b28c1dac887c9a21874932261be7
vslvskyy <princess05102001@gmail.com>
2022-11-26 22:08:31 +0300

    Add dataset

---
+++ dataset/buffer_dataset.py
@@ -0,0 +1,76 @@
+import torch
+from torch.utils.data import Dataset
+
+
+class BufferDataset(Dataset):
+    def __init__(self, buffer):
+        self.buffer = buffer
+        self.length_dataset = len(self.buffer)
+
+    def __len__(self):
+        return self.length_dataset
+
+    def __getitem__(self, idx):
+        return self.buffer[idx]
+
+def reprocess_tensor(batch, cut_list):
+    texts = [batch[ind]["text"] for ind in cut_list]
+    mel_targets = [batch[ind]["mel_target"] for ind in cut_list]
+    durations = [batch[ind]["duration"] for ind in cut_list]
+    pitches = [batch[ind]["pitch"] for ind in cut_list]
+    # pitch_specs = [batch[ind]["pitch_spec"] for ind in cut_list]
+    # pitch_stats = [
+    #     torch.tensor([
+    #         batch[ind]["pitch_mean"], batch[ind]["pitch_std"]
+    #     ]) for ind in cut_list
+    # ]
+    # pitch_stds = [batch[ind]["pitch"][3] for ind in cut_list]
+    energies = [batch[ind]["energy"] for ind in cut_list]
+
+    length_text = np.array([])
+    for text in texts:
+        length_text = np.append(length_text, text.size(0))
+
+    src_pos = list()
+    max_len = int(max(length_text))
+    for length_src_row in length_text:
+        src_pos.append(np.pad([i+1 for i in range(int(length_src_row))],
+                              (0, max_len-int(length_src_row)), 'constant'))
+    src_pos = torch.from_numpy(np.array(src_pos))
+
+    length_mel = np.array(list())
+    for mel in mel_targets:
+        length_mel = np.append(length_mel, mel.size(0))
+
+    mel_pos = list()
+    max_mel_len = int(max(length_mel))
+    for length_mel_row in length_mel:
+        mel_pos.append(np.pad([i+1 for i in range(int(length_mel_row))],
+                              (0, max_mel_len-int(length_mel_row)), 'constant'))
+    mel_pos = torch.from_numpy(np.array(mel_pos))
+
+    texts = pad_1D_tensor(texts)
+    durations = pad_1D_tensor(durations)
+    mel_targets = pad_2D_tensor(mel_targets)
+    pitches = pad_1D_tensor(pitches)
+    # pitch_specs = pad_2D_tensor(pitch_specs)
+    # pitch_stats = pad_1D_tensor(pitch_stats)
+    energies = pad_1D_tensor(energies)
+
+
+    out = {"text": texts,
+           "mel_target": mel_targets,
+           "duration": durations,
+           "mel_pos": mel_pos,
+           "src_pos": src_pos,
+           "mel_max_len": max_mel_len,
+           "pitch": pitches,
+          #  "pitch_spec": pitch_specs,
+          #  "pitch_stats": pitch_stats,
+           "energy": energies
+    }
+
+    return out
+
+
+
